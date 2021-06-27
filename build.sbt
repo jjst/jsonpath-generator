7f54948724d962aeb3ea637fc6d3b1297a74cdc8
Jeremie Jost <jeremiejost@gmail.com>
2021-06-27 08:32:42 +0200

    Change so far

--- build.sbt
+++ build.sbt
@@ -7,3 +7,11 @@ name := "jsonpath-generator"
 libraryDependencies ++= Seq(
   "org.json" % "json" % "20210307"
 )
+
+val circeVersion = "0.14.1"
+
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
--- src/main/scala/GeneratorApp.scala
+++ src/main/scala/GeneratorApp.scala
@@ -1,25 +1,70 @@
 import org.json.JSONObject
 
 object GeneratorApp extends App {
-  println("hi")
+  val json = new JSONObject()
+  println(JsonPathExpression.Empty(json).text)
+  println(JsonPathExpression.Empty(json).nextValidTokens)
 }
 
-case class JsonPathExpression(json: JSONObject, tokens: Seq[JsonPathToken]) {
+class JsonPathExpression(json: JSONObject, tokens: List[JsonPathToken]) {
+  lazy val nextValidTokens: Set[JsonPathToken] = {
+    if (json.isEmpty) {
+      Set.empty
+    }
+    else if (tokens.isEmpty) {
+      Set(JsonPathToken.Begin)
+    } else {
+      ???
+    }
+  }
 
+  def add(token: JsonPathToken): JsonPathExpression =
+    ???
+
+  def text: String = {
+    tokens.map(_.text).mkString("")
+  }
 }
-
-sealed trait JsonPathToken
-
-object Begin extends JsonPathToken
-case class SelectField(fieldName: String) extends JsonPathToken
-case class IndexArray(index: ArrayIndex)
-
-sealed trait ArrayIndex
-case class IndexValue(idx: Int) extends ArrayIndex
-case class IndexRange(from: Int, to: Int) extends ArrayIndex
-case object IndexWildCard extends ArrayIndex
 
 object JsonPathExpression {
   def Empty(json: JSONObject): JsonPathExpression =
-    JsonPathExpression(json, Seq.empty)
+    new JsonPathExpression(json, List.empty)
+}
+
+sealed trait JsonPathToken {
+  def text: String
+}
+
+object JsonPathToken {
+  object Begin extends JsonPathToken {
+    val text = "$"
+  }
+
+  case class SelectField(fieldName: String) extends JsonPathToken {
+    val text = s".$fieldName"
+  }
+
+  case class IndexArray(index: ArrayIndex) extends JsonPathToken {
+    val text = {
+      val idxText = index match {
+        case ArrayIndex.Selection(idx) =>
+          idx.mkString(",")
+        case ArrayIndex.Range(from, to) =>
+          s"$from:$to"
+        case ArrayIndex.Wildcard =>
+          "*"
+      }
+      s"[$idxText]"
+    }
+  }
+}
+
+sealed trait ArrayIndex
+object ArrayIndex {
+  // Can be negative (last element), and should be a list of values (el selection)
+  case class Selection(idx: Seq[Int]) extends ArrayIndex
+
+  case class Range(from: Int, to: Int) extends ArrayIndex
+
+  case object Wildcard extends ArrayIndex
 }
